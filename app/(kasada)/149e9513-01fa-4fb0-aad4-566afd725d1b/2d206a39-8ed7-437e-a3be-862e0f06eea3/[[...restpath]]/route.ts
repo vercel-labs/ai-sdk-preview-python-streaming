7c072b77fa00f9806fac6490b45aa5e5288ab99a
jeremyphilemon <git@jrmyphlmn.com>
2024-07-29 15:29:47 +0530

    Remove edge

--- app/(kasada)/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/[[...restpath]]/route.ts
+++ app/(kasada)/149e9513-01fa-4fb0-aad4-566afd725d1b/2d206a39-8ed7-437e-a3be-862e0f06eea3/[[...restpath]]/route.ts
@@ -1,7 +1,3 @@
-export const runtime = "edge";
-export const dynamic = "force-dynamic";
-export const maxDuration = 3;
-
 async function handler(request: Request) {
   const url = new URL(request.url);
 
