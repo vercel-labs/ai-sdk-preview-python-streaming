53ccf54b864d88866d5f0316b5fe4e86eb1ff4d6
jeremyphilemon <git@jrmyphlmn.com>
2024-07-29 17:29:56 +0530

    Stream responses

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ from .utils.prompt import ClientMessage, convert_to_openai_messages
 from typing import List
 from dotenv import load_dotenv
 from .utils.tools import get_current_weather
+from fastapi.responses import StreamingResponse
 
 
 load_dotenv(".env.local")
@@ -27,6 +28,11 @@ available_tools = {
 }
 
 
+async def fake_video_streamer():
+    for i in range(50):
+        yield b"some fake video bytes"
+
+
 @app.post("/api/chat")
 def hello_world(request: Request):
     messages = request.messages
@@ -35,53 +41,55 @@ def hello_world(request: Request):
     completion = client.chat.completions.create(
         messages=openai_messages,
         model="gpt-4o",
-        tools=[{
-            "type": "function",
-            "function": {
-                "name": "get_current_weather",
-                "description": "Get the current weather in a given location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "location": {
-                            "type": "string",
-                            "description": "The city and state, e.g. San Francisco, CA",
-                        },
-                        "unit": {
-                            "type": "string",
-                            "enum": ["celsius", "fahrenheit"]},
-                    },
-                    "required": ["location",],
-                },
-            },
-        }]
+        # tools=[{
+        #     "type": "function",
+        #     "function": {
+        #         "name": "get_current_weather",
+        #         "description": "Get the current weather in a given location",
+        #         "parameters": {
+        #             "type": "object",
+        #             "properties": {
+        #                 "location": {
+        #                     "type": "string",
+        #                     "description": "The city and state, e.g. San Francisco, CA",
+        #                 },
+        #                 "unit": {
+        #                     "type": "string",
+        #                     "enum": ["celsius", "fahrenheit"]},
+        #             },
+        #             "required": ["location",],
+        #         },
+        #     },
+        # }]
     )
+
+    return StreamingResponse(fake_video_streamer())
 
     for choice in completion.choices:
         if choice.message.tool_calls is None:
             return choice.message.content
 
-        elif choice.message.tool_calls:
-            openai_messages.append(choice.message)
+        # elif choice.message.tool_calls:
+        #     openai_messages.append(choice.message)
 
-            for tool_call in choice.message.tool_calls:
-                name = tool_call.function.name
-                args = json.loads(tool_call.function.arguments)
+        #     for tool_call in choice.message.tool_calls:
+        #         name = tool_call.function.name
+        #         args = json.loads(tool_call.function.arguments)
 
-                tool_response = available_tools[name](**args)
+        #         tool_response = available_tools[name](**args)
 
-                openai_messages.append({
-                    "tool_call_id": tool_call.id,
-                    "role": "tool",
-                    "name": name,
-                    "content": json.dumps(tool_response),
-                })
+        #         openai_messages.append({
+        #             "tool_call_id": tool_call.id,
+        #             "role": "tool",
+        #             "name": name,
+        #             "content": json.dumps(tool_response),
+        #         })
 
-            completion_with_tool_result = client.chat.completions.create(
-                messages=openai_messages,
-                model="gpt-4o",
-            )
+        #     completion_with_tool_result = client.chat.completions.create(
+        #         messages=openai_messages,
+        #         model="gpt-4o",
+        #     )
 
-            for choice in completion_with_tool_result.choices:
-                if choice.message.tool_calls is None:
-                    return choice.message.content
+        #     for choice in completion_with_tool_result.choices:
+        #         if choice.message.tool_calls is None:
+        #             return choice.message.content
