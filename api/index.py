1bb3b80e65cb9a583f14a96f17cc0284c12f77c6
jeremyphilemon <git@jrmyphlmn.com>
2025-10-20 21:37:10 -0700

    add logging

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 import json
+import traceback
 from typing import List
 from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
 from pydantic import BaseModel
@@ -46,69 +47,73 @@ available_tools = {
 }
 
 def stream_text(client: OpenAI, messages: List[ChatCompletionMessageParam], protocol: str = 'data'):
-    draft_tool_calls = []
-    draft_tool_calls_index = -1
+    try:
+        draft_tool_calls = []
+        draft_tool_calls_index = -1
 
-    stream = client.chat.completions.create(
-        messages=messages,
-        model="gpt-4o",
-        stream=True,
-        tools=TOOL_DEFINITIONS,
-    )
+        stream = client.chat.completions.create(
+            messages=messages,
+            model="gpt-4o",
+            stream=True,
+            tools=TOOL_DEFINITIONS,
+        )
 
-    for chunk in stream:
-        for choice in chunk.choices:
-            if choice.finish_reason == "stop":
-                continue
+        for chunk in stream:
+            for choice in chunk.choices:
+                if choice.finish_reason == "stop":
+                    continue
 
-            elif choice.finish_reason == "tool_calls":
-                for tool_call in draft_tool_calls:
-                    yield '9:{{"toolCallId":"{id}","toolName":"{name}","args":{args}}}\n'.format(
-                        id=tool_call["id"],
-                        name=tool_call["name"],
-                        args=tool_call["arguments"])
+                elif choice.finish_reason == "tool_calls":
+                    for tool_call in draft_tool_calls:
+                        yield '9:{{"toolCallId":"{id}","toolName":"{name}","args":{args}}}\n'.format(
+                            id=tool_call["id"],
+                            name=tool_call["name"],
+                            args=tool_call["arguments"])
 
-                for tool_call in draft_tool_calls:
-                    tool_result = available_tools[tool_call["name"]](
-                        **json.loads(tool_call["arguments"]))
+                    for tool_call in draft_tool_calls:
+                        tool_result = available_tools[tool_call["name"]](
+                            **json.loads(tool_call["arguments"]))
 
-                    yield 'a:{{"toolCallId":"{id}","toolName":"{name}","args":{args},"result":{result}}}\n'.format(
-                        id=tool_call["id"],
-                        name=tool_call["name"],
-                        args=tool_call["arguments"],
-                        result=json.dumps(tool_result))
-                draft_tool_calls = []
-                draft_tool_calls_index = -1
+                        yield 'a:{{"toolCallId":"{id}","toolName":"{name}","args":{args},"result":{result}}}\n'.format(
+                            id=tool_call["id"],
+                            name=tool_call["name"],
+                            args=tool_call["arguments"],
+                            result=json.dumps(tool_result))
+                    draft_tool_calls = []
+                    draft_tool_calls_index = -1
 
-            elif choice.delta.tool_calls:
-                for tool_call in choice.delta.tool_calls:
-                    id = tool_call.id
-                    name = tool_call.function.name
-                    arguments = tool_call.function.arguments
+                elif choice.delta.tool_calls:
+                    for tool_call in choice.delta.tool_calls:
+                        id = tool_call.id
+                        name = tool_call.function.name
+                        arguments = tool_call.function.arguments
 
-                    if (id is not None):
-                        draft_tool_calls_index += 1
-                        draft_tool_calls.append(
-                            {"id": id, "name": name, "arguments": ""})
+                        if (id is not None):
+                            draft_tool_calls_index += 1
+                            draft_tool_calls.append(
+                                {"id": id, "name": name, "arguments": ""})
 
-                    else:
-                        draft_tool_calls[draft_tool_calls_index]["arguments"] += arguments
+                        else:
+                            draft_tool_calls[draft_tool_calls_index]["arguments"] += arguments
 
-            else:
-                if choice.delta.content is not None:
-                    yield '0:{text}\n'.format(text=json.dumps(choice.delta.content))
+                else:
+                    if choice.delta.content is not None:
+                        yield '0:{text}\n'.format(text=json.dumps(choice.delta.content))
 
-        if chunk.choices == [] and chunk.usage is not None:
-            usage = chunk.usage
-            prompt_tokens = usage.prompt_tokens
-            completion_tokens = usage.completion_tokens
+            if chunk.choices == [] and chunk.usage is not None:
+                usage = chunk.usage
+                prompt_tokens = usage.prompt_tokens
+                completion_tokens = usage.completion_tokens
 
-            yield 'e:{{"finishReason":"{reason}","usage":{{"promptTokens":{prompt},"completionTokens":{completion}}},"isContinued":false}}\n'.format(
-                reason="tool-calls" if len(
-                    draft_tool_calls) > 0 else "stop",
-                prompt=prompt_tokens,
-                completion=completion_tokens
-            )
+                yield 'e:{{"finishReason":"{reason}","usage":{{"promptTokens":{prompt},"completionTokens":{completion}}},"isContinued":false}}\n'.format(
+                    reason="tool-calls" if len(
+                        draft_tool_calls) > 0 else "stop",
+                    prompt=prompt_tokens,
+                    completion=completion_tokens
+                )
+    except Exception:
+        traceback.print_exc()
+        raise
 
 
 
